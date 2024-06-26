412d1041b0bbfd56664b53059a63c2cff1cb427d
aaronchen <aaronbychen@gmail.com>
2024-06-26 13:37:34 +0800

    userService finished

--- src/main/java/com/aaron/usercenter/service/UserService.java
+++ src/main/java/com/aaron/usercenter/service/UserService.java
@@ -2,6 +2,7 @@ package com.aaron.usercenter.service;
 
 import com.aaron.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * 用户服务
@@ -14,11 +15,20 @@ public interface UserService extends IService<User> {
     /**
      * 用户注释
      *
-     * @param userAccount 用户账户
-     * @param userPassword 用户密码
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
      * @param checkPassword 校验密码
      * @return 新用户id
      */
     long userRegister(String userAccount, String userPassword, String checkPassword);
 
+    /**
+     * 用户登录
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param request 请求
+     * @return 脱敏后的用户信息
+     */
+    User doLogin(String userAccount, String userPassword, HttpServletRequest request);
 }
--- src/main/java/com/aaron/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/aaron/usercenter/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.aaron.usercenter.service.impl;
 
+import java.util.Date;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.aaron.usercenter.model.domain.User;
 import com.aaron.usercenter.service.UserService;
 import com.aaron.usercenter.mapper.UserMapper;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -21,16 +25,28 @@ import java.util.regex.Pattern;
  * @createDate 2024-06-23 20:09:30
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements UserService{
+        implements UserService {
 
     @Resource
     private UserMapper userMapper;
+
+    /**
+     * 盐值，混淆密码
+     */
+    private static final String SALT = "aaron";
+
+    /**
+     * 用户登录态键
+     */
+    private static final String USER_LOGIN_STATE = "userLoginState";
 
     @Override
     public long userRegister(String userAccount, String userPassword, String checkPassword) {
         // 1. 校验
         if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            // todo 修改为自定义异常
             return -1;
         }
         if (userAccount.length() < 4) {
@@ -46,7 +62,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         // 密码和校验密码相同
-        // 有bug记得检查变量名
         if (!userPassword.equals(checkPassword)) {
             return -1;
         }
@@ -58,7 +73,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         // 2. 加密
-        final String SALT = "aaron";
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
         // 3. 插入数据
         User user = new User();
@@ -69,5 +83,51 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         return user.getId();
+    }
+
+    @Override
+    public User doLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        // 1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            return null;
+        }
+        if (userAccount.length() < 4) {
+            return null;
+        }
+        if (userPassword.length() < 8) {
+            return null;
+        }
+        // 账户不能包含特殊字符
+        String inValidPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*()——+|{}【】‘；：”“’。，、？\\\\\\\\]";
+        Matcher matcher = Pattern.compile(inValidPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return null;
+        }
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        // 查询用户是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        queryWrapper.eq("userPassword", encryptPassword);
+        User user = userMapper.selectOne(queryWrapper);
+        // 用户不存在
+        if (user == null) {
+            log.info("user login failed, userAccount cannot match with userPassword");
+            return null;
+        }
+        // 3. 用户脱敏
+        User safetyUser = new User();
+        safetyUser.setId(user.getId());
+        safetyUser.setUsername(user.getUsername());
+        safetyUser.setUserAccount(user.getUserAccount());
+        safetyUser.setAvatarUrl(user.getAvatarUrl());
+        safetyUser.setGender(user.getGender());
+        safetyUser.setPhone(user.getPhone());
+        safetyUser.setEmail(user.getEmail());
+        safetyUser.setUserStatus(user.getUserStatus());
+        safetyUser.setCreateTime(user.getCreateTime());
+        // 4. 记录用户的登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE, safetyUser);
+        return safetyUser;
     }
 }
